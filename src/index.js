18210cfcc0a3b8842b7516e5d196612ade580648
Kyle Miskell <klabweb@tuta.io>
2023-10-19 16:39:24 -0400

    lost the old git from when I made this in 2018. New git from 2023-10-18 adding all files.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IntervalBoxGroup from './IntervalBoxGroup';
+import './index.css';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<IntervalBoxGroup/>, document.getElementById('root'));
+
+
+//serviceWorker.unregister();
