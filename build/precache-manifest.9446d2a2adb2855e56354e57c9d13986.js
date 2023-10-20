5e4aceabbcbc5631216f539b3da692b4bbc65f3d
Kyle Miskell <klabweb@tuta.io>
2023-10-19 20:16:42 -0400

    add build for prod deploy

---
+++ build/precache-manifest.9446d2a2adb2855e56354e57c9d13986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "072c801097ae8f054068",
+    "url": "/react-stocks/static/css/main.8421123e.chunk.css"
+  },
+  {
+    "revision": "072c801097ae8f054068",
+    "url": "/react-stocks/static/js/main.072c8010.chunk.js"
+  },
+  {
+    "revision": "5053061e62c1e29fb877",
+    "url": "/react-stocks/static/js/1.5053061e.chunk.js"
+  },
+  {
+    "revision": "ec23229268a08a8a6ba5",
+    "url": "/react-stocks/static/js/runtime~main.ec232292.js"
+  },
+  {
+    "revision": "8a8aa2d10ccbbb81608ecb5e7c847500",
+    "url": "/react-stocks/index.html"
+  }
+];
